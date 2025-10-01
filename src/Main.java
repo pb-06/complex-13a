813afabe625cb748aed970698d9879bf0cf9ac15
pb-06 <kkandomail@gmail.com>
2025-10-01 12:49:37 +0200

    Add menu loop options

--- src/Main.java
+++ src/Main.java
@@ -2,91 +2,95 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Menu");
-        System.out.println("-".repeat(32));
-        System.out.println("1. a + b");
-        System.out.println("2. a - b");
-        System.out.println("3. a * b");
-        System.out.println("4. |z|");
-        System.out.println("5. arg(z)");
-        System.out.println("0. Exit program");
+        char choice;
+        do {
+            System.out.println("Menu");
+            System.out.println("-".repeat(32));
+            System.out.println("1. a + b");
+            System.out.println("2. a - b");
+            System.out.println("3. a * b");
+            System.out.println("4. |z|");
+            System.out.println("5. arg(z)");
+            System.out.println("0. Exit program");
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Choose (0-5): ");
-        char choice = input.nextLine().charAt(0);
-        switch (choice) {
-            case '1' : {
-                System.out.print("a: ");
-                String aStr = input.nextLine();
-                Complex a = new Complex(aStr);
-                System.out.print("b: ");
-                String bStr = input.nextLine();
-                Complex b = new Complex(bStr);
+            Scanner input = new Scanner(System.in);
+            System.out.println("Choose (0-5): ");
+            choice = input.nextLine().charAt(0);
+            switch (choice) {
+                case '1': {
+                    System.out.print("a: ");
+                    String aStr = input.nextLine();
+                    Complex a = new Complex(aStr);
+                    System.out.print("b: ");
+                    String bStr = input.nextLine();
+                    Complex b = new Complex(bStr);
 
-                Complex c = a;
-                c = c.add(b);
+                    Complex c = a;
+                    c = c.add(b);
 
-                System.out.println(aStr + " + " + bStr + " = " + c);
+                    System.out.println(aStr + " + " + bStr + " = " + c);
 
-                break;
+                    break;
+                }
+                case '2': {
+                    System.out.print("a: ");
+                    String aStr = input.nextLine();
+                    Complex a = new Complex(aStr);
+                    System.out.print("b: ");
+                    String bStr = input.nextLine();
+                    Complex b = new Complex(bStr);
+
+                    Complex c = a;
+                    c = c.subtract(b);
+
+                    System.out.println(aStr + " - " + bStr + " = " + c);
+
+                    break;
+                }
+                case '3': {
+                    System.out.print("a: ");
+                    String aStr = input.nextLine();
+                    Complex a = new Complex(aStr);
+                    System.out.print("b: ");
+                    String bStr = input.nextLine();
+                    Complex b = new Complex(bStr);
+
+                    Complex c = a;
+                    c = c.multiply(b);
+
+                    System.out.println(aStr + " * " + bStr + " = " + c);
+
+                    break;
+                }
+                case '4': {
+                    System.out.print("z: ");
+                    String zStr = input.nextLine();
+                    Complex z = new Complex(zStr);
+
+                    double abs = z.abs();
+
+                    System.out.println("|" + z + "| = " + abs);
+
+                    break;
+                }
+                case '5': {
+                    System.out.print("z: ");
+                    String zStr = input.nextLine();
+                    Complex z = new Complex(zStr);
+
+                    double arg = z.angle();
+
+                    System.out.println("arg (" + z + ") = " + arg);
+
+                    break;
+                }
+                case '0': {
+                    break;
+                }
+                default: {
+                    System.out.println("");
+                }
             }
-            case '2' : {
-                System.out.print("a: ");
-                String aStr = input.nextLine();
-                Complex a = new Complex(aStr);
-                System.out.print("b: ");
-                String bStr = input.nextLine();
-                Complex b = new Complex(bStr);
-
-                Complex c = a;
-                c = c.subtract(b);
-
-                System.out.println(aStr + " - " + bStr + " = " + c);
-
-                break;
-            }
-            case '3' : {
-                System.out.print("a: ");
-                String aStr = input.nextLine();
-                Complex a = new Complex(aStr);
-                System.out.print("b: ");
-                String bStr = input.nextLine();
-                Complex b = new Complex(bStr);
-
-                Complex c = a;
-                c = c.multiply(b);
-
-                System.out.println(aStr + " * " + bStr + " = " + c);
-
-                break;
-            }
-            case '4' : {
-                System.out.print("z: ");
-                String zStr = input.nextLine();
-                Complex z = new Complex(zStr);
-
-                double abs = z.abs();
-
-                System.out.println("|" + z + "| = " + abs);
-
-                break;
-            }
-            case '5' : {
-                System.out.print("z: ");
-                String zStr = input.nextLine();
-                Complex z = new Complex(zStr);
-
-                double arg = z.angle();
-
-                System.out.println("arg (" + z + ") = " + arg);
-
-                break;
-            }
-            case '0' : {
-                break;
-            }
-            default: {}
-        }
-
+        } while (choice != '0');
     }
 }
